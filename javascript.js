7d22fa726cbb0688d170a283f30488dbfca4234d
rameshtripuraneni <tripuraneni.ramesh@gmail.com>
2025-09-15 17:12:45 -0500

    assignment on data types and knowing how to run and change the test

---
+++ javascript.js
@@ -0,0 +1,31 @@
+console.log("Hello, World!")
+let first_name="Ramesh";
+let last_name="Tripuraneni"
+console.log(first_name,  last_name);
+
+
+let age= 26;
+console.log(age);
+age = 27;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+ //pi = 10;
+// console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+console.log(23+97);
+
+
+let a = 10;
+console.log(a);
+a=5;
+let b = 7 * a;
+console.log(b);
+
+const max=57;
+const actual= max -13;
+const percentage= actual/max;
+console.log(percentage);
+//alert(percentage);
